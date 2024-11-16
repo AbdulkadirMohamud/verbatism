8cfb72b89707cacf830b4d8a23df8f90aa219059
Abdulkadir <qansax30@hotmail.com>
2024-11-16 19:56:38 +0100

    Add project files.

---
+++ verbatism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verbatism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //\t\n\" \\ = \
+            string speech = "He said \"something\"";
+            string path = "c:\\Users\\coffeeNCode\\Desktop\\c# course";
+            Console.WriteLine(path);
+            Console.ReadLine();
+        }
+    }
+}
